dcec46b375027f3de2413dbcc9d388cdf1ae052a
damien <anselmidamien@gmail.com>
2019-05-08 13:03:33 +0200

    Updates

---
+++ precache-manifest.3a5529fd4ded908cff4c003c82539a99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045374e4e5c5c974abb44ae356e712e8",
+    "url": "/react-video-player/index.html"
+  },
+  {
+    "revision": "76ea5c93ba14a54baf06",
+    "url": "/react-video-player/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "58eb84de380ee401be47",
+    "url": "/react-video-player/static/css/main.7b0fbf6f.chunk.css"
+  },
+  {
+    "revision": "76ea5c93ba14a54baf06",
+    "url": "/react-video-player/static/js/2.6602a7e7.chunk.js"
+  },
+  {
+    "revision": "58eb84de380ee401be47",
+    "url": "/react-video-player/static/js/main.a96a58cd.chunk.js"
+  },
+  {
+    "revision": "37b3ba558bafe6a2a2aa",
+    "url": "/react-video-player/static/js/runtime~main.d7a5b8cc.js"
+  },
+  {
+    "revision": "f13004eed4251c602bbe15737e8a1ecb",
+    "url": "/react-video-player/static/media/big_buck_bunny.f13004ee.mp4"
+  }
+]);
